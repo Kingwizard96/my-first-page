cf415e4223ee8cdb15eed03b317e4649b7cbda06
Kingwizard96 <calvarado1996@gmail.com>
2023-07-13 23:27:17 -0500

    <Message>

--- assets/script.js
+++ assets/script.js
@@ -39,3 +39,22 @@ console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
 selectTopic();
+var topic = "HTML";
+
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+
+var shapes = ["triangle", "square", "hexagon", "circle"];
+
+for(var x = 0; x < shapes.length; x++){
+    console.log(shapes[x]);
+}
